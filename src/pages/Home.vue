a893d666140b201c750a8ef350be6a69cfd47f3f
Skystedt <hampus@skystedt.se>
2016-11-24 20:05:18 +0100

    Fixed lint errors

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,11 +5,11 @@
 </template>
 
 <script>
-  import MainLayout from '../layouts/Main.vue'
+  import MainLayout from '../layouts/Main.vue';
 
   export default {
     components: {
       MainLayout
     }
-  }
+  };
 </script>
